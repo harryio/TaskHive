a3c71f0b9f72e37484faebb4c0b3f1007ed2d7e0
Harry Singh <me@harryio.com>
2016-06-01 16:20:42 +0530

    Add missing files

--- app/src/main/java/com/harryio/taskhive/ui/ComposeMessageActivity.java
+++ app/src/main/java/com/harryio/taskhive/ui/ComposeMessageActivity.java
@@ -1,14 +1,27 @@
 package com.harryio.taskhive.ui;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 import com.dpizarro.autolabel.library.AutoLabelUI;
+import com.dpizarro.autolabel.library.Label;
 import com.harryio.taskhive.R;
+import com.harryio.taskhive.ui.model.Task;
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -31,9 +44,26 @@ public class ComposeMessageActivity extends AppCompatActivity {
     EditText keywordEdittext;
     @BindView(R.id.keyword_view)
     AutoLabelUI keywordView;
+    @BindView(R.id.task_description_editText)
+    TextInputEditText taskDescriptionEditText;
+    @BindView(R.id.task_category)
+    TextInputEditText taskCategory;
+    @BindView(R.id.bitcoin_checkbox)
+    CheckBox bitcoinCheckbox;
+    @BindView(R.id.paypal_checkbox)
+    CheckBox paypalCheckbox;
+    @BindView(R.id.dogecoin_checkbox)
+    CheckBox dogecoinCheckbox;
+    @BindView(R.id.add_keyword_button)
+    Button addKeywordButton;
+    @BindView(R.id.post_job_button)
+    Button postJobButton;
+    @BindView(R.id.task_description_textInputLayout)
+    TextInputLayout taskDescriptionTextInputLayout;
+    @BindView(R.id.task_category_textInputLayout)
+    TextInputLayout taskCategoryTextInputLayout;
 
-    private String jobType = JOB_TYPE_OFFER;
-    private String paymentMethod = PAYMENT_METHOD_BITCOIN;
+    private String taskType = JOB_TYPE_OFFER;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,46 +84,85 @@ public class ComposeMessageActivity extends AppCompatActivity {
         });
     }
 
-    @OnClick({R.id.offer_button, R.id.request_button, R.id.add_keyword_button, R.id.post_job_button,
-            R.id.bitcoin_button, R.id.paypal_button, R.id.dogecoin_button})
+    @OnClick({R.id.offer_button, R.id.request_button, R.id.add_keyword_button, R.id.post_job_button})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.offer_button:
                 offerButton.setChecked(true);
-                jobType = JOB_TYPE_OFFER;
+                taskType = JOB_TYPE_OFFER;
                 break;
 
             case R.id.request_button:
                 requestButton.setChecked(true);
-                jobType = JOB_TYPE_REQUEST;
-                break;
-
-            case R.id.bitcoin_button:
-                paymentMethod = PAYMENT_METHOD_BITCOIN;
-                break;
-
-            case R.id.paypal_button:
-                paymentMethod = PAYMENT_METHOD_PAYPAL;
-                break;
-
-            case R.id.dogecoin_button:
-                paymentMethod = PAYMENT_METHOD_DOGECOIN;
+                taskType = JOB_TYPE_REQUEST;
                 break;
 
             case R.id.add_keyword_button:
                 String keyword = keywordEdittext.getText().toString();
-                keywordView.addLabel(keyword);
-                keywordEdittext.setText("");
+                if (!TextUtils.isEmpty(keyword)) {
+                    keywordView.addLabel(keyword);
+                    keywordEdittext.setText("");
+                }
                 break;
 
             case R.id.post_job_button:
                 postJob();
                 break;
         }
-
     }
 
     private void postJob() {
+        taskDescriptionTextInputLayout.setErrorEnabled(false);
+        taskDescriptionTextInputLayout.setError("");
+        taskCategoryTextInputLayout.setErrorEnabled(false);
+        taskCategoryTextInputLayout.setError("");
 
+        String description = taskDescriptionEditText.getText().toString();
+        String category = taskCategory.getText().toString();
+
+        if (TextUtils.isEmpty(description)) {
+            taskDescriptionTextInputLayout.setErrorEnabled(true);
+            taskDescriptionTextInputLayout.setError("Task description cannot be null");
+            return;
+        } else if (TextUtils.isEmpty(category)) {
+            taskCategoryTextInputLayout.setErrorEnabled(true);
+            taskCategoryTextInputLayout.setError("Task category cannot be empty");
+            return;
+        }
+
+        List<String> paymentMethods = new ArrayList<>(3);
+        if (bitcoinCheckbox.isChecked()) {
+            paymentMethods.add(PAYMENT_METHOD_BITCOIN);
+        }
+
+        if (paypalCheckbox.isChecked()) {
+            paymentMethods.add(PAYMENT_METHOD_PAYPAL);
+        }
+
+        if (dogecoinCheckbox.isChecked()) {
+            paymentMethods.add(PAYMENT_METHOD_DOGECOIN);
+        }
+
+        if (paymentMethods.size() == 0) {
+            Toast.makeText(this, "Please select at least one payment method", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        List<Label> labels = keywordView.getLabels();
+        if (labels.size() == 0) {
+            Toast.makeText(this, "Please add at least one task keyword", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        List<String> keywords = new ArrayList<>(labels.size());
+        for (Label label : labels) {
+            keywords.add(label.getText());
+        }
+
+        Task task = new Task(taskType, description, paymentMethods, Integer.parseInt(category), keywords);
+        Moshi moshi = new Moshi.Builder().build();
+        JsonAdapter<Task> jsonAdapter = moshi.adapter(Task.class);
+
+        String taskJson = jsonAdapter.toJson(task);
     }
 }
